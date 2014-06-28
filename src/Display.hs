416391ea3c65e452ad1a617165627cca1c3dbf26
Crockeo <crockeo@gmail.com>
2014-06-28 13:59:10 -0400

    Added a way to display the board with vty.

---
+++ src/Display.hs
@@ -0,0 +1,26 @@
+module Display where
+
+import Graphics.Vty.Attributes
+import Graphics.Vty.Image
+
+import Board
+
+-- Generating styling for a Tile
+generateAttr :: Tile -> Attr
+generateAttr Player = defAttr `withForeColor` green
+generateAttr Spawn  = defAttr `withForeColor` green
+generateAttr Empty  = defAttr `withForeColor` black
+generateAttr Wall   = defAttr `withForeColor` white
+generateAttr Finish = defAttr `withForeColor` blue
+
+-- Generating an image for a line of tiles
+generateLine :: [Tile] -> Image
+generateLine =
+  horizCat . generateLine'
+  where generateLine' :: [Tile] -> [Image]
+        generateLine' []     = []
+        generateLine' (x:xs) = char (generateAttr x) (displayTile x) : generateLine' xs
+
+-- Generating an image for a board
+generateBoardImage :: Board -> Image
+generateBoardImage (Board tss) = vertCat $ map generateLine tss
