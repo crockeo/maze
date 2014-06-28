274d2e8c935d0da84df8615068bc79cc46d23ae5
Crockeo <crockeo@gmail.com>
2014-06-28 13:58:41 -0400

    Created a board datatype.

---
+++ src/Board.hs
@@ -0,0 +1,35 @@
+module Board where
+
+import Utils
+
+data Tile = Player
+          | Spawn
+          | Empty
+          | Wall
+          | Finish
+
+newtype Board = Board { tiles :: [[Tile]] }
+
+-- Converting a tile to its display char
+displayTile :: Tile -> Char
+displayTile Player = '@'
+displayTile Spawn  = '!'
+displayTile Empty  = '.'
+displayTile Wall   = '#'
+displayTile Finish = '^'
+
+-- Converting a char to a tile
+readTile :: Char -> Tile
+readTile '@' = Player
+readTile '!' = Spawn
+readTile '.' = Empty
+readTile '#' = Wall
+readTile '^' = Finish
+
+-- Reading a board
+readBoard :: String -> Board
+readBoard str = Board $  map (map readTile) $ lines str
+
+-- Loading a board from file
+loadBoard :: FilePath -> IO Board
+loadBoard fp = readFile fp >>= return . readBoard
